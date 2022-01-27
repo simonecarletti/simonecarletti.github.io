3044651682e74e5778224397f6725339a2e0f349
regi18 <regi18@outlook.it>
2022-01-27 20:53:49 +0100

    updated website

--- js/chunk-vendors~0af256c1-legacy.cfa66dff.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-vendors~0af256c1"],[]]);
---
+++ js/chunk-vendors~6301a11c.320493fb.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-vendors~6301a11c"],[]]);
